2c96fd2eb6946827cf10b3e9cc7dc281e0e1e6cc
Martin E. Liza <mliza@email.arizona.edu>
2024-11-24 20:52:52 -0500

    Updated main.py

--- tecplot/tecplot_tools.py
+++ tecplot/tecplot_tools.py
@@ -4,6 +4,7 @@ from tecplot.exception import *
 from tecplot.constant import *
 import sys 
 import os 
+import pickle
 import IPython 
 
 # Uncomment the following line to connect to a running instance of Tecplot 360:
@@ -42,3 +43,40 @@ def data_cutter(struct_in):
     dataset = tp.active_frame().dataset
     tp.data.save_tecplot_ascii(file_out_abs_path, dataset=dataset,
                                 zones=line, use_point_format=True)
+
+    tp.new_layout()
+
+def tp_to_pickle(file_in_abs_path, file_out_abs_path):
+    files_in          = os.listdir(file_in_abs_path) 
+    data_dict         = { }
+
+    # Load multiple files in one pickle file 
+    for f in files_in: 
+        f_name = f.split('.')[0]
+        data_in             = tp.data.load_tecplot(os.path.join(file_in_abs_path, f)) 
+        data_variable_names = data_in.variable_names
+        zone_names          = data_in.zone_names 
+
+        # Check if there is only 1 zone or multiple zones and 
+        # initializes dictionary accordingly 
+        if len(zone_names) == 1:
+            data_dict[f_name] = { }
+            z = zone_names[0] 
+        else:
+            for z in zone_names:
+                data_dict[f_name][z] = { }
+        
+        # Load variables in the dictionary dict[file_in_name][zone][variable] 
+        for v in data_variable_names:
+            if len(zone_names) == 1:
+                data_dict[f_name][v] = data_in.zone(z).values(v)[:] 
+            else: 
+                for z in zone_names:
+                    data_dict[f_name][z][v] = data_in.zone(z).values(v)[:] 
+        # Clear tecplot 
+        tp.new_layout()
+
+    # Store data in a dictionary 
+    pickle_out = open(file_out_abs_path, 'wb')
+    pickle.dump(data_dict, pickle_out) 
+    pickle_out.close() 
--- tecplot/tp_main.py
+++ tecplot/tp_main.py
@@ -11,15 +11,23 @@ if __name__ == "__main__":
 
     struct_in = { }
     struct_in['x1'] = -0.01
-    struct_in['x2'] = 0.0
+    struct_in['x2'] = 0.01
     struct_in['y1'] = 0.0
     struct_in['y2'] = 0.0
-    struct_in['n'] = 100
+    struct_in['n'] = 200
     
+    # Creates stagnation data
     for i in folders_in:
         struct_in['f_in'] = os.path.join(su2_output_path, i, 'flow.szplt')
         struct_in['f_out'] = os.path.join(data_output_path, f'{i}_stagnation.dat')
         tp_tools.data_cutter(struct_in)
+
+    # Converts stagnation data to pickle files
+    tp_tools.tp_to_pickle(data_output_path,
+                os.path.join(data_output_path,'stagnation.pickle'))
+
+
+
 
 
 
